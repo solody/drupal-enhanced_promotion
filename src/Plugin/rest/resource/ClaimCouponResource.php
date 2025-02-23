27a35a008a11955826a3c97c90b6a30fafe8ebcc
kent <164713332@qq.com>
2025-02-23 14:47:35 +0000

    Add ClaimCouponResource.

--- src/Plugin/rest/resource/ClaimCouponResource.php
+++ src/Plugin/rest/resource/ClaimCouponResource.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace Drupal\enhanced_promotion\Plugin\rest\resource;
 
-use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
-use Drupal\Core\KeyValueStore\KeyValueStoreInterface;
+use Drupal\commerce_promotion\CouponCodeGeneratorInterface;
+use Drupal\commerce_promotion\CouponCodePattern;
+use Drupal\commerce_promotion\Entity\PromotionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\rest\Attribute\RestResource;
 use Drupal\rest\ModifiedResourceResponse;
 use Drupal\rest\Plugin\ResourceBase;
-use Drupal\rest\ResourceResponse;
+use Drupal\user\UserInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Route;
 
 /**
@@ -51,11 +53,6 @@ use Symfony\Component\Routing\Route;
 final class ClaimCouponResource extends ResourceBase {
 
   /**
-   * The key-value storage.
-   */
-  private readonly KeyValueStoreInterface $storage;
-
-  /**
    * {@inheritdoc}
    */
   public function __construct(
@@ -64,10 +61,10 @@ final class ClaimCouponResource extends ResourceBase {
     $plugin_definition,
     array $serializer_formats,
     LoggerInterface $logger,
-    KeyValueFactoryInterface $keyValueFactory,
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+    private readonly CouponCodeGeneratorInterface $couponCodeGenerator,
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
-    $this->storage = $keyValueFactory->get('enhanced_promotion_claim_coupon');
   }
 
   /**
@@ -80,16 +77,47 @@ final class ClaimCouponResource extends ResourceBase {
       $plugin_definition,
       $container->getParameter('serializer.formats'),
       $container->get('logger.factory')->get('rest'),
-      $container->get('keyvalue')
+      $container->get('entity_type.manager'),
+      $container->get('commerce_promotion.coupon_code_generator'),
     );
   }
 
   /**
    * Responds to POST requests and saves the new record.
    */
-  public function post(array $data): ModifiedResourceResponse {
-    // Return the newly created record in the response body.
-    return new ModifiedResourceResponse($data, 201);
+  public function post(PromotionInterface $promotion, array $data): ModifiedResourceResponse {
+    // Check the limitation.
+    if (!$promotion->requiresCoupon()) {
+      throw new BadRequestHttpException('The promotion can not has any coupons.');
+    }
+    $coupons = $promotion->getCoupons();
+    if (count($coupons) >= $promotion->getUsageLimit()) {
+      throw new BadRequestHttpException('The promotion touch usage limitation.');
+    }
+    $coupons_of_current_user = [];
+    foreach ($coupons as $coupon) {
+      $owner = $coupon->get('user_id')->entity;
+      if ($owner instanceof UserInterface && (int) \Drupal::currentUser()->id() === (int) $owner->id()) {
+        $coupons_of_current_user[] = $coupon;
+      }
+    }
+    if (count($coupons_of_current_user) >= $promotion->getCustomerUsageLimit()) {
+      throw new BadRequestHttpException('The promotion touch customer usage limitation.');
+    }
+
+    /** @var \Drupal\commerce_promotion\CouponStorageInterface $coupon_storage */
+    $coupon_storage = $this->entityTypeManager->getStorage('commerce_promotion_coupon');
+    $pattern = new CouponCodePattern('alphanumeric', '', '', 18);
+    $codes = $this->couponCodeGenerator->generateCodes($pattern, 5);
+    $new_coupon = $coupon_storage->create([
+      'user_id' => \Drupal::currentUser()->id(),
+      'code' => reset($codes),
+      'promotion_id' => $promotion->id(),
+      'usage_limit' => 1,
+      'usage_limit_customer' => 1,
+    ]);
+    $new_coupon->save();
+    return new ModifiedResourceResponse($new_coupon, 201);
   }
 
   /**
